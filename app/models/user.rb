1058efb2f2ef2f13d6b2de7befee6eea9ae5ef11
Paul Iogna <paul.iogna@gmail.com>
2016-02-02 15:11:26 -0500

    Initial Commit

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ActiveRecord::Base
+
+  has_many :reviews
+
+  has_secure_password
+
+  validates :email, presence: true
+
+  validates :firstname, presence: true
+
+  validates :lastname, presence: true
+
+  validates :password, length: { in: 6..20 }, on: :create
+
+  def firstname_last_initial
+    "#{firstname} #{lastname[0,1]}"
+  end
+end
