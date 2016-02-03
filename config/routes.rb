4c03b9ce3e1bd0832a6c0830f05cf7b022d56069
Paul Iogna <paul.iogna@gmail.com>
2016-02-03 15:16:33 -0500

    Implements singular session resource

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
-  resources :sessions, only: [:new, :create, :destroy]
+  resource :session, only: [:new, :create, :destroy]
 
   resources :movies do
     get 'search', :on => :collection
